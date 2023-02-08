a8dd87766dda71de34aad237ed7f0ae94206e387
Hugo Robitaillie <hugo@la-ligne-web.com>
2023-02-08 17:17:06 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Fragment } from "react"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from "./components/Layout/Header"
+import Layout from "./components/Layout/Layout"
+import NewEmployee from "./pages/NewEmployee"
+import Team from "./pages/Team"
+
+export default function App() {
+
+  return (
+    <Fragment >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />} >
+            <Route index element={<NewEmployee />}/>
+            <Route path="/team" element={<Team/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
+  )
+}
